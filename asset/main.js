0cbfe3ca31e38f78396f872153c734a7996b3f88
Rafaolez <129889693+Rafaolez@users.noreply.github.com>
2023-05-01 01:23:42 -0300

    bummmm

--- asset/main.js
+++ asset/main.js
@@ -1,7 +1,6 @@
 function Numeros(){
     let i = 0;
     while (i < 100) {
-        i++;
             let h1 = document.createElement( "h1" );
             h1.innerHTML = i;
             document.getElementById("Resposta1").append(h1);
